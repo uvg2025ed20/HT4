8418126194f13fce228b851af8179b98d373f2e7
PabloVS044 <pvasquezs044@gmail.com>
2025-02-24 19:34:41 -0600

    Correcciones en estructura

--- src/main/java/uvg/edu/IListADT.java
+++
@@ -1,7 +0,0 @@
-package uvg.edu;
-
-public interface IListADT<T> {
-    void add(T item);
-    T remove();
-    boolean isEmpty();
-}
--- src/main/java/uvg/edu/IStack.java
+++
@@ -1,8 +0,0 @@
-package uvg.edu;
-
-public interface IStack<T> {
-    void push(T item);
-    T pop();
-    T peek();
-    boolean isEmpty();
-}
